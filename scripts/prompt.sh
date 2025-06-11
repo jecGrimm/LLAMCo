9f067b3f25539064be75cc344f84cbe272828187
Jana <janagrimm22@gmail.com>
2025-06-11 11:10:59 +0200

    ADDED slurm prompt script

---
+++ scripts/prompt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#SBATCH --job-name=prompt_llama
+#SBATCH --comment="Prompt the whole dataset."
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=j.grimm@campus.lmu.de
+#SBATCH --chdir=/home/g/grimmj/LLAMCO
+#SBATCH --output=/home/g/grimmj/LLAMCO/scripts/output/slurm.%j.%N.out
+#SBATCH --ntasks=1
+
+python3 -u prompt.py
