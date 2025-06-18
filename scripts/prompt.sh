00b3316a836b86e32ce84dc3ed66a21efeb112b7
Jana Grimm <grimmj@citrin.cip.ifi.lmu.de>
2025-06-18 08:36:35 +0200

    ADDED test selection

--- scripts/prompt.sh
+++ scripts/prompt.sh
@@ -4,8 +4,8 @@
 #SBATCH --comment="Prompt the whole dataset."
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=j.grimm@campus.lmu.de
-#SBATCH --chdir=/home/g/grimmj/LLAMCO
-#SBATCH --output=/home/g/grimmj/LLAMCO/scripts/output/slurm.%j.%N.out
+#SBATCH --chdir=/home/g/grimmj/LLAMCo
+#SBATCH --output=/home/g/grimmj/LLAMCo/scripts/output/slurm.%j.%N.out
 #SBATCH --ntasks=1
 
 python3 -u prompt.py
