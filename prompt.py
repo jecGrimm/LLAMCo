aedd913ea056f28330d6f4b5450033db949f643f
Jana <janagrimm22@gmail.com>
2025-07-17 13:21:02 +0200

    ADDED evaluation llama 8B prompt 10

--- prompt.py
+++ prompt.py
@@ -113,8 +113,6 @@ Wichtige Hinweise:
     "in_B-v-Wiese": "",
     "in_RUB_Sammlung": ""
 }
-
-Hinweise: "ED" steht für "Erstdruck", "Hg." bezieht sich auf den Herausgeber der Anthologie oder Almanachs etc., in dem der Text ggf. erschienen ist. "in_Pantheon" bedeutet, dass der Text in der Novellensammlung "Pantheon" erschienen ist. "in_B-v-Wiese" bedeutet, dass der Text in "Benno von Wiese: Die deutsche Novelle von Goethe bis Kafka. Interpretationen" erschienen ist. "In_RUB_Sammlung" bedeutet, dass der Text in "Erzählungen und Novellen des 19. Jahrhunderts" im Reclam-Verlag erschienen ist.
 """
 
 def create_messages(prompt, system_prompt = DEFAULT_SYSTEM_PROMPT_DE):
