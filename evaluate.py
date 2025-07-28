c897dea4d3b9233538fe704860a93134dd48b9f7
Jana <janagrimm22@gmail.com>
2025-07-28 20:12:55 +0200

    ADDED evaluation of 0 and 1 shot llama 70b

--- evaluate.py
+++ evaluate.py
@@ -198,7 +198,7 @@ def validate_samples(eval_samples, model_out, cols):
                             total_tp += 1
                             row_tp[idx] += 1
                             col_tp[col] += 1
-                        elif col == "Kanon_Status" and str(model_val).lower().strip() == "kanonisch" and str(prepped_sample[col]).lower().strip() in ["2", "3"]:
+                        elif col == "Kanon_Status" and str(model_val).lower().strip() in ["kanonisch", "kanonisiert", "Kanon"] and str(prepped_sample[col]).lower().strip() in ["2", "3"]:
                             # TODO: schauen wegen nicht kanonisch
                             total_tp += 1
                             row_tp[idx] += 1
@@ -300,7 +300,8 @@ def plot_cols(experiment_mode = "test", model_id = "Llama3_8B"):
         col_recall[shot] = [metrics["recall"] for metrics in col_metrics.values()]
     
     df = pd.DataFrame(data = col_recall, index = col_metrics.keys(), columns = ["0", "1", "5"])
-    df.plot.bar()
+    df.plot.bar(ylabel = "Recall", xlabel = "Column", title = f"Recall per column: {model_id}")
+    plt.legend(title = "Shots", loc=(0.3, 0.48), fontsize = "x-small", title_fontsize = "x-small")
     plt.tight_layout()
     plt.savefig(f"{path}/evaluation_{model_id}_{experiment_mode}_cols.png")
 
@@ -312,6 +313,6 @@ if __name__=="__main__":
     experiment_mode = parser.parse_args().experiment_mode
 
     #evaluate_wiki()
-    #evaluate_llm(model_id=model_name, experiment_mode=experiment_mode)
+    evaluate_llm(model_id=model_name, experiment_mode=experiment_mode)
 
-    plot_cols(model_id=model_name, experiment_mode=experiment_mode)
+    #plot_cols(model_id=model_name, experiment_mode=experiment_mode)
--- helper.py
+++ helper.py
@@ -1,26 +1,38 @@
-from prompt import load_ckp
 import json
 
-# ckp_file_1 = "output/Llama3_70B/test/0/answers_Llama3_70B_test_0_ckp_120.json"
-# ckp_file_2 = "output/Llama3_70B/test/0/answers_Llama3_70B_test_0_ckp_580.json"
+def merge_ckpts():
+    ckp_file_1 = "output/Llama3_70B/test/1/outputs_Llama3_70B_test_1_ckp.json"
+    ckp_file_2 = "output/Llama3_70B/test/1/outputs_Llama3_70B_test_1_old.json"
 
-ckp_file_1 = "output/Llama3_70B/test/0/outputs_Llama3_70B_test_0_ckp_120.json"
-ckp_file_2 = "output/Llama3_70B/test/0/outputs_Llama3_70B_test_0_ckp_580.json"
+    with open(ckp_file_1, "r", encoding = "utf-8") as f:
+        ckp_data_1 = json.load(f)
 
-with open(ckp_file_1, "r", encoding = "utf-8") as f:
-    #print("loading")
-    ckp_data_1 = json.load(f)
+    with open(ckp_file_2, "r", encoding = "utf-8") as f:
+        #print("loading")
+        ckp_data_2 = json.load(f)
 
-with open(ckp_file_2, "r", encoding = "utf-8") as f:
-    #print("loading")
-    ckp_data_2 = json.load(f)
-#ckp_data_1 = load_ckp(ckp_file_1)
-#ckp_data_2 = load_ckp(ckp_file_2)
+    ckp_data = ckp_data_1 | ckp_data_2
 
-ckp_data = ckp_data_1 | ckp_data_2
+    with open("output/Llama3_70B/test/1/outputs_Llama3_70B_test_1.json", "w", encoding = "utf-8") as f:
+        json.dump(ckp_data, f, indent=4)
 
-# with open("output/Llama3_70B/test/0/answers_Llama3_70B_test_0_ckp.json", "w", encoding = "utf-8") as f:
-#     json.dump(ckp_data, f, indent=4)
+def count_model_dicts():
+    model_out_file = f"output/Llama3_70B/test/1/outputs_Llama3_70B_test_1.json"
 
-with open("output/Llama3_70B/test/0/outputs_Llama3_70B_test_0_ckp.json", "w", encoding = "utf-8") as f:
-    json.dump(ckp_data, f, indent=4)
+    with open(model_out_file, 'r', encoding="utf-8") as f:
+        model_out = json.load(f)
+
+    empt_counter = 0
+    full_counter = 0
+    for model_dict in model_out.values():
+        if model_dict == "":
+            empt_counter += 1
+        else:
+            full_counter += 1
+
+    print("Empty rows: ", empt_counter)
+    print("Full rows: ", full_counter)
+
+if __name__ == "__main__":
+    merge_ckpts()
+    count_model_dicts()
